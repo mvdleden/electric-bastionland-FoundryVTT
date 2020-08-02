d204e904e8b7f33d6342ef93f910b296cefb411c
menno <menno@q33.nl>
2020-08-02 20:44:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Electric Bastionland system for FoundryVTT!
+
+[Electric Bastionland](https://bastionlandpress.com/products/electric-bastionland-hardback-book)
+
+This system is based on the [Into The Odd system](https://github.com/voidcase/IntoTheOdd-FoundryVTT/blob/master/README.md) for FoundryVTT
---
+++ module/item/item-sheet.js
@@ -0,0 +1,57 @@
+/**
+ * Extend the basic ItemSheet with some very simple modifications
+ * @extends {ItemSheet}
+ */
+export class ElectricBastionlandItemSheet extends ItemSheet {
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["electricbastionland", "sheet", "item"],
+      width: 420,
+      height: 320
+    });
+  }
+
+  /** @override */
+  get template() {
+    const path = "systems/electricbastionland/templates/item";
+    // Return a single sheet for all item types.
+    return `${path}/item-sheet.html`;
+    // Alternatively, you could use the following return statement to do a
+    // unique item sheet by type, like `weapon-sheet.html`.
+
+    // return `${path}/${this.item.data.type}-sheet.html`;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    return data;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  setPosition(options = {}) {
+    const position = super.setPosition(options);
+    const sheetBody = this.element.find(".sheet-body");
+    const bodyHeight = position.height - 192;
+    sheetBody.css("height", bodyHeight);
+    return position;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Roll handlers, click handlers, etc. would go here.
+  }
+}
